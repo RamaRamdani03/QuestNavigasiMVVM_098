6e10ee5bc82b2a29247598e3ae68390e4df08a83
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-21 22:55:54 +0700

    `Menambahkan data class DataMahasiswa untuk menyimpan informasi mahasiswa`

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataMahasiswa.kt
@@ -0,0 +1,10 @@
+package com.example.arsitekturmvvm.model
+
+data class DataMahasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val email: String= "",
+    val alamat: String = "",
+    val noHp: String= "",
+    val NIM: String= "",
+)
