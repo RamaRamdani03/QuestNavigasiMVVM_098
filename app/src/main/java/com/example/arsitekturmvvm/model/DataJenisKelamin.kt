2f084371383231b40f0b6cdf7be0691fa1c64731
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-21 22:50:38 +0700

    `Menambahkan DataJenisKelamin untuk daftar jenis kelamin sebagai sumber data`

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataJenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.arsitekturmvvm.model
+
+import com.example.arsitekturmvvm.R
+
+object DataJenisKelamin {
+    val listjk = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
