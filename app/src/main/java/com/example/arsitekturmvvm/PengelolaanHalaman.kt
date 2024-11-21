6f752b6b55e87df017354a94130f907b248d38f1
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-21 23:15:38 +0700

    `Menambahkan enum class Halaman untuk menentukan jenis halaman aplikasi`

---
+++ app/src/main/java/com/example/arsitekturmvvm/PengelolaanHalaman.kt
@@ -0,0 +1,24 @@
+package com.example.arsitekturmvvm
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvvm.ui.viewmodel.MahasiswaViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.arsitekturmvvm.model.DataJenisKelamin
+import com.example.arsitekturmvvm.ui.view.DetailMahasiswaView
+import com.example.arsitekturmvvm.ui.view.FormMahasiswaView
+
+enum class Halaman{
+    Formulir,
+    Detail,
+}
